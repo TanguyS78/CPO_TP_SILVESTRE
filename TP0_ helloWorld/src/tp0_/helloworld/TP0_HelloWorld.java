5d06d0010c3057e6865a1a01d0d3befc208ec80b
Tanguy <tanguy.silvestre@epfedu.fr>
2025-10-22 16:08:47 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,27 @@
+/* TP0_ helloWorld S'exercer,20/10/2025 
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SILVESTRE Richard
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ;
+        System.out.println( " Au revoir " ) ;
+        int i = 20;
+        String prenom; 
+        Scanner sc ; 
+        sc = new Scanner (System.in) ; 
+        System.out.println("Quel est votre prénom ?"); 
+        prenom = sc.nextLine();  
+ }
+    
+}
