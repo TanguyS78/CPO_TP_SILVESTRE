8f1b10b3a23ea49348b89dfb5b72d3a1177b3e70
Tanguy <tanguy.silvestre@epfedu.fr>
2025-10-24 11:12:55 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_stats_Silvestre_Noshie/src/tp1_stats_silvestre_noshie/TP1_stats_Silvestre_Noshie.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_silvestre_noshie;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SILVESTRE Richard
+ */
+public class TP1_stats_Silvestre_Noshie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] nombre;
+        nombre= new int[6];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choisissez un nombre entier");
+        int Var1=sc.nextInt();
+    
+}
