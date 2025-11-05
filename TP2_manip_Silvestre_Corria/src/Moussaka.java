894beed1893dc17a4b2ba5cd41206c311f2fa1c1
Tanguy <tanguy.silvestre@epfedu.fr>
2025-11-05 09:50:55 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_manip_Silvestre_Corria/src/Moussaka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SILVESTRE Richard
+ */
+public class Moussaka {
+    int nbCalories;
+    
+     public Moussaka(int nbCalories) {
+        this.nbCalories = nbCalories;
+    }
+}
---
+++ TP2_manip_Silvestre_Corria/src/Tartiflette.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SILVESTRE Richard
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette (int nbCalories){
+        this.nbCalories = nbCalories;
+    }
+}
