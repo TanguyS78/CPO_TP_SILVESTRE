9b1c902fca6701c6d36978edda66c1a791635577
Tanguy <tanguy.silvestre@epfedu.fr>
2025-12-17 10:33:40 +0100

    test de la class cellule

---
+++ Mini_projet_demineur/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SILVESTRE Richard
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Nous écrirons le test ici
+        Cellule maCellule = new Cellule();
+        System.out.println(maCellule);
+
+    }
+}
