df131bd3091e9f2d6ada427b3e6a82ba29fbaefc
Tanguy <tanguy.silvestre@epfedu.fr>
2025-12-19 10:46:35 +0100

    main plus de case pour jouer au jeu

--- Mini_projet_demineur/src/Main.java
+++ Mini_projet_demineur/src/Main.java
@@ -9,40 +9,11 @@
  */
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
-        // 1. Paramètres du jeu
-        int lignes = 5;
-        int colonnes = 5;
-        int bombes = 3;
-
-        // 2. Création et préparation de la grille
-        GrilleDeJeu grille = new GrilleDeJeu(lignes, colonnes, bombes);
-        grille.placerBombesAleatoirement();
-        grille.calculerBombesAdjacentes();
-
-        Scanner scanner = new Scanner(System.in);
-        boolean fini = false;
-
-        System.out.println("=== DÉMINEUR ===");
-
-        // 3. Boucle de jeu
-        while (!fini) {
-            // Afficher la grille
-            System.out.println(grille.toString());
-
-            // Demander les coordonnées
-            System.out.print("Entrez la LIGNE : ");
-            int l = scanner.nextInt();
-            System.out.print("Entrez la COLONNE : ");
-            int c = scanner.nextInt();
-
-            // Révéler la case
-            grille.revelerCellule(l, c);
-
-            // Vérifier fin de partie
-            if (grille.estPerdu()) {
-                System.out.println(grille.toString()); // On affiche la grille finale
-                System.out.println("BOOM ! Vous avez perdu ! 💥");
-                fini = true;
-        
+        // On lance simplement la fenêtre graphique
+        // (5 lignes, 5 colonnes, 3 bombes)
+        new FenetreJeu(5, 5, 3);
+    }
+}
