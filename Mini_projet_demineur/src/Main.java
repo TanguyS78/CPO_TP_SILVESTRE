58484c5b34c2f1c91402b3b873f13579b393daa7
Tanguy <tanguy.silvestre@epfedu.fr>
2025-12-19 10:09:29 +0100

    réalisation du main pour lancer le jeu

--- Mini_projet_demineur/src/Main.java
+++ Mini_projet_demineur/src/Main.java
@@ -7,11 +7,50 @@
  *
  * @author SILVESTRE Richard
  */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Nous écrirons le test ici
-        Cellule maCellule = new Cellule();
-        System.out.println(maCellule);
+        // 1. Paramètres du jeu
+        int lignes = 5;
+        int colonnes = 5;
+        int bombes = 3;
 
+        // 2. Création et préparation de la grille
+        GrilleDeJeu grille = new GrilleDeJeu(lignes, colonnes, bombes);
+        grille.placerBombesAleatoirement();
+        grille.calculerBombesAdjacentes();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean fini = false;
+
+        System.out.println("=== DÉMINEUR ===");
+
+        // 3. Boucle de jeu
+        while (!fini) {
+            // Afficher la grille
+            System.out.println(grille.toString());
+
+            // Demander les coordonnées
+            System.out.print("Entrez la LIGNE : ");
+            int l = scanner.nextInt();
+            System.out.print("Entrez la COLONNE : ");
+            int c = scanner.nextInt();
+
+            // Révéler la case
+            grille.revelerCellule(l, c);
+
+            // Vérifier fin de partie
+            if (grille.estPerdu()) {
+                System.out.println(grille.toString()); // On affiche la grille finale
+                System.out.println("BOOM ! Vous avez perdu ! 💥");
+                fini = true;
+            } else if (grille.estGagne()) {
+                System.out.println(grille.toString());
+                System.out.println("FÉLICITATIONS ! Vous avez gagné ! 🏆");
+                fini = true;
+            }
+        }
+        scanner.close();
     }
 }
